b097daa86303a0431de0db545593abb8a9179263
zhenghemin <zhenghemin@rr.tv>
2023-03-06 21:13:55 +0800

    2

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
-const express = require('express')
-const router = require('./router')
-const app = express()
-const cors = require('cors')
-// const customMiddleware = require('./utils/middleWare')
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
-app.use(cors())
-// app.use(customMiddleware.handleResponseHeader)
-app.use('/api', router)
-app.listen(80, function(){
-  console.log('服务已启动');
+// const express = require('express')
+// const router = require('./router')
+// const app = express()
+// const cors = require('cors')
+// // const customMiddleware = require('./utils/middleWare')
+// app.use(express.json())
+// app.use(express.urlencoded({extended: false}))
+// app.use(cors())
+// // app.use(customMiddleware.handleResponseHeader)
+// app.use('/api', router)
+// app.listen(80, function(){
+//   console.log('服务已启动');
+// })
+
+const mysql = require('mysql')
+
+mysql.createPool({
+  host: '127.0.0.1', // 数据库ip地址
+  user: 'root',
+  password: '123456',
+  database: 'my_db_01'
 })
